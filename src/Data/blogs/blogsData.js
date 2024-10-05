60f5cdf8e29ed13b84bbb1d5f3be95965afeec81
irshad <sanuirshad00@gmail.com>
2024-10-05 15:19:49 +0530

    first commit

--- src/Data/blogs/blogsData.js
+++ src/Data/blogs/blogsData.js
@@ -203,200 +203,200 @@ const blogData = [
   },
   {
     id: 2,
-    title: "UNVEILING SAUDI ARABIA; TOP TRAVEL DESTINATIONS YOU CAN'T MISS!",
+    title: "Coming Soon",
     small_thumb: assets.Blog1,
 
     content: [
-      {
-        subheading: "Investing in Riyadh",
-        section_heading: "Top Places to Visit in Riyadh:",
-        text: "From budgeting to outlining the itinerary, planning vacations are equally exhausting and overwhelming. A major part of the work goes into deciding the right destination, we can help you out here!",
-        places: [
-          {
-            name: "King Abdulaziz Historical Center",
-            details:
-              "Dive into Saudi Arabia’s past with exhibitions that showcase the nation’s cultural and historical journey.",
-          },
-          {
-            name: "Edge of The World",
-            details:
-              "For adventure seekers, this breathtaking cliff offers stunning views of the endless desert horizon.",
-          },
-          {
-            name: "Kingdom Centre Tower",
-            details:
-              "An iconic skyscraper, where visitors can enjoy panoramic views of the city.",
-          },
-        ],
-      },
-      {
-        subheading: "Investing in Diriyah",
-        section_heading: "Top Places to Visit in Diriyah:",
+      // {
+      //   subheading: "Investing in Riyadh",
+      //   section_heading: "Top Places to Visit in Riyadh:",
+      //   text: "From budgeting to outlining the itinerary, planning vacations are equally exhausting and overwhelming. A major part of the work goes into deciding the right destination, we can help you out here!",
+      //   places: [
+      //     {
+      //       name: "King Abdulaziz Historical Center",
+      //       details:
+      //         "Dive into Saudi Arabia’s past with exhibitions that showcase the nation’s cultural and historical journey.",
+      //     },
+      //     {
+      //       name: "Edge of The World",
+      //       details:
+      //         "For adventure seekers, this breathtaking cliff offers stunning views of the endless desert horizon.",
+      //     },
+      //     {
+      //       name: "Kingdom Centre Tower",
+      //       details:
+      //         "An iconic skyscraper, where visitors can enjoy panoramic views of the city.",
+      //     },
+      //   ],
+      // },
+      // {
+      //   subheading: "Investing in Diriyah",
+      //   section_heading: "Top Places to Visit in Diriyah:",
 
-        text: "Known as the original home of the Saudi royal family, Diriyah is a place where history and culture come alive. Wander through this UNESCO World Heritage site to explore the mud-brick fortifications and traditional Najdi architecture.",
-        places: [
-          {
-            name: "At-Turaif",
-            details:
-              "A UNESCO World Heritage Site in Diriyah, At-Turaif features historic mudbrick buildings and museums, offering insights into the Al Saud royal family and Diriyah’s rich heritage.",
-          },
-          {
-            name: "Al Bujairi Heritage Park",
-            details:
-              "This vibrant park in Diriyah offers rides, markets, food stalls, and activities like pottery.",
-          },
-          {
-            name: "Wadi Hanifah",
-            details:
-              "Perfect for nature lovers, Wadi Hanifah offers hiking trails, local flora, and historic sites like ancient wells.",
-          },
-        ],
-      },
-      {
-        subheading: "Investing in Makkah",
-        section_heading: "Top Places to Visit in Makkah:",
-        text: "Makkah, known as Mecca, is the spiritual heart of Saudi Arabia and the holiest city in Islam, drawing millions of pilgrims annually.",
-        places: [
-          {
-            name: "Jabal Al Noor",
-            details:
-              "The Mountain of Light, where Prophet Muhammad received his first revelation.",
-          },
-          {
-            name: "Masjid al-Haram",
-            details:
-              "Experience the profound spiritual ambiance surrounding the Kaaba.",
-          },
-          {
-            name: "Kaaba",
-            details:
-              "The most sacred site in Islam, located at the heart of the Grand Mosque.",
-          },
-          {
-            name: "Abraj Al-Bait Tower",
-            details:
-              "A towering skyscraper that’s also home to the Makkah Royal Clock.",
-          },
-          {
-            name: "Mount Arafat",
-            details:
-              "Known as the Mountain of Mercy, it’s a key site during the Hajj pilgrimage.",
-          },
-        ],
-      },
-      {
-        subheading: "Investing in Madinah",
-        section_heading: "Top Places to Visit in Madinah:",
+      //   text: "Known as the original home of the Saudi royal family, Diriyah is a place where history and culture come alive. Wander through this UNESCO World Heritage site to explore the mud-brick fortifications and traditional Najdi architecture.",
+      //   places: [
+      //     {
+      //       name: "At-Turaif",
+      //       details:
+      //         "A UNESCO World Heritage Site in Diriyah, At-Turaif features historic mudbrick buildings and museums, offering insights into the Al Saud royal family and Diriyah’s rich heritage.",
+      //     },
+      //     {
+      //       name: "Al Bujairi Heritage Park",
+      //       details:
+      //         "This vibrant park in Diriyah offers rides, markets, food stalls, and activities like pottery.",
+      //     },
+      //     {
+      //       name: "Wadi Hanifah",
+      //       details:
+      //         "Perfect for nature lovers, Wadi Hanifah offers hiking trails, local flora, and historic sites like ancient wells.",
+      //     },
+      //   ],
+      // },
+      // {
+      //   subheading: "Investing in Makkah",
+      //   section_heading: "Top Places to Visit in Makkah:",
+      //   text: "Makkah, known as Mecca, is the spiritual heart of Saudi Arabia and the holiest city in Islam, drawing millions of pilgrims annually.",
+      //   places: [
+      //     {
+      //       name: "Jabal Al Noor",
+      //       details:
+      //         "The Mountain of Light, where Prophet Muhammad received his first revelation.",
+      //     },
+      //     {
+      //       name: "Masjid al-Haram",
+      //       details:
+      //         "Experience the profound spiritual ambiance surrounding the Kaaba.",
+      //     },
+      //     {
+      //       name: "Kaaba",
+      //       details:
+      //         "The most sacred site in Islam, located at the heart of the Grand Mosque.",
+      //     },
+      //     {
+      //       name: "Abraj Al-Bait Tower",
+      //       details:
+      //         "A towering skyscraper that’s also home to the Makkah Royal Clock.",
+      //     },
+      //     {
+      //       name: "Mount Arafat",
+      //       details:
+      //         "Known as the Mountain of Mercy, it’s a key site during the Hajj pilgrimage.",
+      //     },
+      //   ],
+      // },
+      // {
+      //   subheading: "Investing in Madinah",
+      //   section_heading: "Top Places to Visit in Madinah:",
 
-        text: "Madinah, the second holiest city in Islam, offers tranquility and religious significance. The city is rich in historical sites and offers stunning views for pilgrims and visitors alike.",
-        places: [
-          {
-            name: "Masjid an-Nabawi",
-            details:
-              "Explore the second holiest mosque, known for its stunning green dome and serene atmosphere.",
-          },
-          {
-            name: "Quba Mosque",
-            details:
-              "Visit the first mosque in Islam, renowned for its historical importance and peaceful environment.",
-          },
-        ],
-      },
-      {
-        subheading: "Investing in Al Ahsa",
-        section_heading: "Top Places to Visit in Ahsa:",
+      //   text: "Madinah, the second holiest city in Islam, offers tranquility and religious significance. The city is rich in historical sites and offers stunning views for pilgrims and visitors alike.",
+      //   places: [
+      //     {
+      //       name: "Masjid an-Nabawi",
+      //       details:
+      //         "Explore the second holiest mosque, known for its stunning green dome and serene atmosphere.",
+      //     },
+      //     {
+      //       name: "Quba Mosque",
+      //       details:
+      //         "Visit the first mosque in Islam, renowned for its historical importance and peaceful environment.",
+      //     },
+      //   ],
+      // },
+      // {
+      //   subheading: "Investing in Al Ahsa",
+      //   section_heading: "Top Places to Visit in Ahsa:",
 
-        text: "Celebrated for its lush landscapes and rich history, Al Ahsa is one of the world’s largest natural agricultural oases with over 3 million palm trees.",
-        places: [
-          {
-            name: "Al Ahsa Oasis",
-            details:
-              "A UNESCO World Heritage site featuring verdant palm groves and historic irrigation systems.",
-          },
-          {
-            name: "Qasr Ibrahim",
-            details:
-              "Explore this ancient fort, an excellent example of traditional Arabian military architecture.",
-          },
-        ],
-      },
-      {
-        subheading: "Investing in Jeddah",
-        section_heading: "Top Places to Visit in Jeddah:",
+      //   text: "Celebrated for its lush landscapes and rich history, Al Ahsa is one of the world’s largest natural agricultural oases with over 3 million palm trees.",
+      //   places: [
+      //     {
+      //       name: "Al Ahsa Oasis",
+      //       details:
+      //         "A UNESCO World Heritage site featuring verdant palm groves and historic irrigation systems.",
+      //     },
+      //     {
+      //       name: "Qasr Ibrahim",
+      //       details:
+      //         "Explore this ancient fort, an excellent example of traditional Arabian military architecture.",
+      //     },
+      //   ],
+      // },
+      // {
+      //   subheading: "Investing in Jeddah",
+      //   section_heading: "Top Places to Visit in Jeddah:",
 
-        text: "Jeddah is a vibrant city on the Red Sea coast, known for its art galleries, historic sites, and stunning waterfront.",
-        places: [
-          {
-            name: "Nassif House Museum",
-            details:
-              "A UNESCO World Heritage site featuring verdant palm groves and historic irrigation systems.",
-          },
-          {
-            name: "Historic Jeddah (Al-Balad)",
-            details:
-              "Discover the traditional charm of Al-Balad with its coralstone buildings and bustling souks.",
-          },
-          {
-            name: "King Fahd Fountain",
-            details:
-              "Admire the world’s tallest fountain, especially captivating when illuminated at night.",
-          },
-          {
-            name: "Jeddah Corniche",
-            details:
-              "Enjoy a leisurely stroll along this picturesque waterfront.",
-          },
-        ],
-      },
-      {
-        subheading: "Investing in Dammam",
-        section_heading: "Top Places to Visit in Dammam:",
+      //   text: "Jeddah is a vibrant city on the Red Sea coast, known for its art galleries, historic sites, and stunning waterfront.",
+      //   places: [
+      //     {
+      //       name: "Nassif House Museum",
+      //       details:
+      //         "A UNESCO World Heritage site featuring verdant palm groves and historic irrigation systems.",
+      //     },
+      //     {
+      //       name: "Historic Jeddah (Al-Balad)",
+      //       details:
+      //         "Discover the traditional charm of Al-Balad with its coralstone buildings and bustling souks.",
+      //     },
+      //     {
+      //       name: "King Fahd Fountain",
+      //       details:
+      //         "Admire the world’s tallest fountain, especially captivating when illuminated at night.",
+      //     },
+      //     {
+      //       name: "Jeddah Corniche",
+      //       details:
+      //         "Enjoy a leisurely stroll along this picturesque waterfront.",
+      //     },
+      //   ],
+      // },
+      // {
+      //   subheading: "Investing in Dammam",
+      //   section_heading: "Top Places to Visit in Dammam:",
 
-        text: "Dammam is known for its modern amenities and scenic waterfronts, blending history with modern life.",
-        places: [
-          {
-            name: "Tarut Castle",
-            details:
-              "Located on Tarut Island, this ancient fortress is one of the oldest in the region.",
-          },
-          {
-            name: "Dammam Museum",
-            details:
-              "Highlights artifacts from archaeological excavations in the area.",
-          },
-          {
-            name: "Dammam Corniche",
-            details:
-              "A scenic waterfront perfect for a leisurely stroll or family picnic.",
-          },
-          {
-            name: "Half Moon Bay",
-            details: "Relax on the sandy shores and enjoy water sports.",
-          },
-          {
-            name: "King Fahd Park",
-            details:
-              "A vast urban park with lush greenery and recreational facilities.",
-          },
-        ],
-      },
-      {
-        subheading: "Investing in Abha",
-        section_heading: "Top Places to Visit in Abha:",
+      //   text: "Dammam is known for its modern amenities and scenic waterfronts, blending history with modern life.",
+      //   places: [
+      //     {
+      //       name: "Tarut Castle",
+      //       details:
+      //         "Located on Tarut Island, this ancient fortress is one of the oldest in the region.",
+      //     },
+      //     {
+      //       name: "Dammam Museum",
+      //       details:
+      //         "Highlights artifacts from archaeological excavations in the area.",
+      //     },
+      //     {
+      //       name: "Dammam Corniche",
+      //       details:
+      //         "A scenic waterfront perfect for a leisurely stroll or family picnic.",
+      //     },
+      //     {
+      //       name: "Half Moon Bay",
+      //       details: "Relax on the sandy shores and enjoy water sports.",
+      //     },
+      //     {
+      //       name: "King Fahd Park",
+      //       details:
+      //         "A vast urban park with lush greenery and recreational facilities.",
+      //     },
+      //   ],
+      // },
+      // {
+      //   subheading: "Investing in Abha",
+      //   section_heading: "Top Places to Visit in Abha:",
 
-        text: "Nestled in the Asir Mountains, Abha is a haven for nature lovers, with its cool climate, lush landscapes, and scenic views.",
-        places: [
-          {
-            name: "Asir National Park",
-            details:
-              "A sprawling natural reserve offering hiking trails, picnic spots, and stunning views.",
-          },
-          {
-            name: "Dahna Falls",
-            details: "A picturesque waterfall surrounded by lush greenery.",
-          },
-        ],
-      },
+      //   text: "Nestled in the Asir Mountains, Abha is a haven for nature lovers, with its cool climate, lush landscapes, and scenic views.",
+      //   places: [
+      //     {
+      //       name: "Asir National Park",
+      //       details:
+      //         "A sprawling natural reserve offering hiking trails, picnic spots, and stunning views.",
+      //     },
+      //     {
+      //       name: "Dahna Falls",
+      //       details: "A picturesque waterfall surrounded by lush greenery.",
+      //     },
+      //   ],
+      // },
     ],
     date: "21-04-2024",
     author: "Mohammed",
